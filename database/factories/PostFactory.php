443816185c744973d8bb7ea1b485170b1b0fb32e
rusanovvs <rusanov.isp@gmail.com>
2024-01-26 11:48:59 +0500

    second

---
+++ database/factories/PostFactory.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Post>
+ */
+class PostFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition()
+    {
+        return [
+            'title' => fake()->words(3, true),
+            'content' => fake()->paragraph(1),
+            'created_at' => fake()->dateTime(),
+            'updated_at' => fake()->dateTime(),
+            'rubric_id' => fake()->numberBetween(1,4),
+        ];
+    }
+}
